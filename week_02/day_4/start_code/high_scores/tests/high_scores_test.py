a57396c602068d02deadc38702f401f1f5541acc
Lewis Bonnar <lewisbonnar74@gmail.com>
2023-03-09 14:12:40 +0000

    first test

---
+++ week_02/day_4/start_code/high_scores/tests/high_scores_test.py
@@ -0,0 +1,22 @@
+import unittest
+
+from src.high_scores import latest, personal_best, personal_top_three
+
+
+class HighScoresTest(unittest.TestCase):
+
+    def test_latest_score(self):
+        self.assertEqual(100, latest([50, 55, 65, 100]))
+
+    
+
+    # Test top three from list of scores
+
+    # Test ordered from highest tp lowest
+
+    # Test top three when there is a tie
+
+    # Test top three when there are less than three
+
+    # Test top three when there is only one
+    
